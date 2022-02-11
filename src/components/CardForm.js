2b2d8c4041d83a14babc17d7219eb335bfc99294
EdgarEsco <ivan_esco10@hotmail.com>
2022-02-10 19:23:32 -0600

    add card form

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,17 +1,9 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class CardForm extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-    state = {  }
-    render() { 
-        return ( 
-            <h5>
-                CARD DE LOS ALUMNOS
-            </h5>
-         );
-    }
+export default function CardForm() {
+  return (
+    <div>
+      <h1>CARD FORM</h1>
+    </div>
+  );
 }
- 
-export default CardForm;
